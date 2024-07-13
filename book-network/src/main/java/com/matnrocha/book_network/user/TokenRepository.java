5c87d06ecd28281baec0bf06fbab6f992f8eea24
matro0 <mateusanroc@gmail.com>
2024-07-13 05:35:49 +0000

    feat: create token repository

--- book-network/src/main/java/com/matnrocha/book_network/user/TokenRepository.java
+++ book-network/src/main/java/com/matnrocha/book_network/user/TokenRepository.java
@@ -1,9 +1,11 @@
 package com.matnrocha.book_network.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByToken(String token);
 }

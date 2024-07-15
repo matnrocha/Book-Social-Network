20a5de2802806083a08c9943a7ab3388e579dfee
matro0 <mateusanroc@gmail.com>
2024-07-15 17:31:27 +0000

    refactor: change visibility of getFullName in User entity

--- book-network/src/main/java/com/matnrocha/book_network/user/User.java
+++ book-network/src/main/java/com/matnrocha/book_network/user/User.java
@@ -90,7 +90,7 @@ public class User implements UserDetails, Principal {
         return enabled;
     }
 
-    private String fullName(){
+    public String getFullName(){
         return firstName + " " + lastName;
     }
 }

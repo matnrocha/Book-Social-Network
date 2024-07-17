ec62f7dcf428e6890638ae2fa244a3bd19d11c48
mateus <mateusanroc@gmail.com>
2024-07-17 18:46:36 -0300

    fix: add a runner for populating the database with first role

--- book-network/src/main/java/com/matnrocha/book_network/BookNetworkApplication.java
+++ book-network/src/main/java/com/matnrocha/book_network/BookNetworkApplication.java
@@ -1,7 +1,11 @@
 package com.matnrocha.book_network;
 
+import com.matnrocha.book_network.role.Role;
+import com.matnrocha.book_network.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,20 @@ public class BookNetworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApplication.class, args);
+	}
+
+	/*
+	Initialize first role USER if not exists
+	 */
+	@Bean
+	public CommandLineRunner runner(
+		RoleRepository roleRepository
+	){
+		return args -> {
+			if(roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
 	}
 
 }

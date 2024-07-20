9ea45dbe68c7786fcda2fbcc3c8fcc3c78709c8f
mateus <mateusanroc@gmail.com>
2024-07-20 10:56:09 -0300

    refactor: makes book and feeback entities extend base entity

--- book-network/src/main/java/com/matnrocha/book_network/book/Book.java
+++ book-network/src/main/java/com/matnrocha/book_network/book/Book.java
@@ -1,18 +1,13 @@
 package com.matnrocha.book_network.book;
 
+import com.matnrocha.book_network.common.BaseEntity;
+import com.matnrocha.book_network.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,11 +15,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Book extends BaseEntity {
+
     private String title;
     private String authorName;
     private String isbn;
@@ -32,19 +24,12 @@ public class Book {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+
     //todo stablish the relationships
 
-    //auditing fields
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
